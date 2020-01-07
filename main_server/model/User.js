8c59abe9b7efa34f535124346ee0994c5046fa24
Kyeongwon <aelleek@gmail.com>
2020-01-07 23:43:32 +0900

    Add model dir and User.js

---
+++ main_server/model/User.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+	id : {
+		type : String,
+		unique : true,
+		lowercase : true,
+		required : [true, "userId must have value"]
+	},
+	password : {
+		type : String,
+		required : [true, "userPwd must have value"],
+		trim : true
+	},
+	nickname : String,
+	permission : {
+		type : Number,
+		required : true
+	},
+	joinDate : {
+		type : Date,
+		default : Date.now
+	},
+	modifiedDate : {
+		type : Date,
+		default : Date.now
+	}
+});
+
+UserSchema.statics.join = function(joinInfo, finalcallback)
+{
+	var user = new this(joinInfo);
+	return user.save()
+};
+
+UserSchema.methods.login = function(loginInfo, finalcallback)
+{
+
+}
+
+module.exports = mongoose.model("User", UserSchema, "users");
